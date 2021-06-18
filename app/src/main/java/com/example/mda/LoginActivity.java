e2e22faae33054b45eb080a330a3b81b7b229d29
Mogarampalli Niharika <mniharika26@gmail.com>
2021-06-18 23:52:10 +0530

    second commit

--- app/src/main/java/com/example/mda/LoginActivity.java
+++ app/src/main/java/com/example/mda/LoginActivity.java
@@ -1,14 +1,28 @@
 package com.example.mda;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.mda.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+
+    ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_login);
+
+        binding.login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
